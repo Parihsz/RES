9d75bceee292c96709c73094bc2905f32b0c9678
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-08-11 01:22:34 -0400

    Update StrMath.lua

--- StrMath.lua
+++ StrMath.lua
@@ -7,65 +7,62 @@ local mt = {}
 -- Helper Functions Dictionary
 local helpers = {}
 
--- Utility: Reverse a string
 function helpers.reverseString(s)
     return s:reverse()
 end
 
--- Utility: Validate a string number
+function helpers.rjust(str, length, char)
+    char = char or "0"
+    return string.rep(char, length - #str) .. str
+end
+
 function helpers.validateNumber(str)
-    if not str:match("^-?%d+$") then
+    if not str:match("^-?%d+%.?%d*$") then
         error("Invalid number string: " .. str)
     end
 end
 
--- Utility: Add a single digit to a string
-function helpers.addSingleDigit(str, digit)
-    local carry = tonumber(digit)
-    local result = {}
-
-    for i = #str, 1, -1 do
-        local sum = tonumber(str:sub(i, i)) + carry
-        carry = math.floor(sum / 10)
-        result[i] = sum % 10
-    end
-
-    if carry > 0 then
-        table.insert(result, 1, carry)
-    end
-
-    return table.concat(result)
+function helpers.splitNumber(str)
+    local integerPart, fractionalPart = str:match("^(-?%d+)(%.%d*)$")
+    fractionalPart = fractionalPart or ""
+    fractionalPart = fractionalPart:sub(2) -- remove the dot
+    return integerPart, fractionalPart
 end
 
--- Utility: Double a string number
-function helpers.addSelf(str)
-    local carry = 0
-    local result = {}
+function helpers.addStrings(str1, str2)
+    local integer1, fraction1 = helpers.splitNumber(str1)
+    local integer2, fraction2 = helpers.splitNumber(str2)
 
-    for i = #str, 1, -1 do
-        local sum = tonumber(str:sub(i, i)) * 2 + carry
-        carry = math.floor(sum / 10)
-        result[i] = sum % 10
-    end
+    local sumFraction = helpers.addStringsOnlyIntegers(fraction1, fraction2)
+    local overflow = math.floor(sumFraction / (10 ^ #fraction1))
+    sumFraction = sumFraction % (10 ^ #fraction1)
 
-    if carry > 0 then
-        table.insert(result, 1, carry)
-    end
+    local sumInteger = helpers.addStringsOnlyIntegers(integer1, integer2) + overflow
 
-    return table.concat(result)
+    return sumInteger .. (sumFraction ~= 0 and ("." .. sumFraction) or "")
 end
 
--- Utility: Generic addition
 function helpers.genericAddition(str1, str2)
-    if #str1 < #str2 then
-        str1, str2 = str2, str1
+    if str1:sub(1, 1) == "-" and str2:sub(1, 1) ~= "-" then
+        return helpers.subtractStrings(str2, str1:sub(2))
+    elseif str1:sub(1, 1) ~= "-" and str2:sub(1, 1) == "-" then
+        return helpers.subtractStrings(str1, str2:sub(2))
+    elseif str1:sub(1, 1) == "-" and str2:sub(1, 1) == "-" then
+        return "-" .. helpers.addStrings(str1:sub(2), str2:sub(2))
+    else
+        return helpers.addStrings(str1, str2)
     end
+end
 
-    str2 = str2 .. string.rep("0", #str1 - #str2)
-    local carry = 0
+function helpers.addStringsOnlyIntegers(str1, str2)
+    local length = math.max(#str1, #str2)
+    str1 = str1:rjust(length, "0")
+    str2 = str2:rjust(length, "0")
+
     local result = {}
+    local carry = 0
 
-    for i = #str1, 1, -1 do
+    for i = length, 1, -1 do
         local sum = tonumber(str1:sub(i, i)) + tonumber(str2:sub(i, i)) + carry
         carry = math.floor(sum / 10)
         result[i] = sum % 10
@@ -78,19 +75,40 @@ function helpers.genericAddition(str1, str2)
     return table.concat(result)
 end
 
--- Utility: Subtract string numbers
 function helpers.subtractStrings(str1, str2)
-    -- Validate if str1 is greater than str2
-    if helpers.compareStrings(str1, str2) < 0 then
-        error("First string must be larger for subtraction")
+    local integer1, fraction1 = helpers.splitNumber(str1)
+    local integer2, fraction2 = helpers.splitNumber(str2)
+
+    if #fraction2 > #fraction1 then
+        fraction1 = fraction1 .. string.rep("0", #fraction2 - #fraction1)
+    elseif #fraction1 > #fraction2 then
+        fraction2 = fraction2 .. string.rep("0", #fraction1 - #fraction2)
     end
+
+    local subFraction = helpers.subtractStringsOnlyIntegers(fraction1, fraction2)
+    local borrow = 0
+
+    if subFraction < 0 then
+        subFraction = subFraction + (10 ^ #fraction1)
+        borrow = 1
+    end
+
+    local subInteger = helpers.subtractStringsOnlyIntegers(integer1, integer2) - borrow
+
+    return subInteger .. (subFraction ~= 0 and ("." .. subFraction) or "")
+end
+
+function helpers.subtractStringsOnlyIntegers(str1, str2)
+    local length = math.max(#str1, #str2)
+    str1 = helpers.rjust(str1, length, "0")
+    str2 = helpers.rjust(str2, length, "0")    
 
     local result = {}
     local borrow = 0
 
-    for i = #str1, 1, -1 do
-        local subtrahend = tonumber(str2:sub(i, i) or 0) + borrow
-        local minuend = tonumber(str1:sub(i, i))
+    for i = length, 1, -1 do
+        local minuend = tonumber(str1:sub(i, i)) - borrow
+        local subtrahend = tonumber(str2:sub(i, i))
         borrow = 0
 
         if minuend < subtrahend then
@@ -102,10 +120,44 @@ function helpers.subtractStrings(str1, str2)
     end
 
     local finalResult = table.concat(result):match("^0*(%d+)$") or "0"
-    return finalResult
+    return tonumber(finalResult)
 end
 
--- Utility: Multiply string by a digit
+function helpers.multiplyStrings(str1, str2)
+    local integer1, fraction1 = helpers.splitNumber(str1)
+    local integer2, fraction2 = helpers.splitNumber(str2)
+
+    local productInteger = helpers.multiplyStringsOnlyIntegers(integer1, integer2)
+    local productFraction1 = helpers.multiplyStringsOnlyIntegers(integer1, fraction2)
+    local productFraction2 = helpers.multiplyStringsOnlyIntegers(integer2, fraction1)
+    local productFraction3 = helpers.multiplyStringsOnlyIntegers(fraction1, fraction2)
+
+    local fractionalLength = #fraction1 + #fraction2
+    local productFraction = helpers.addStringsOnlyIntegers(productFraction1 .. productFraction2, productFraction3)
+
+    local overflow = math.floor(productFraction / (10 ^ fractionalLength))
+    productFraction = productFraction % (10 ^ fractionalLength)
+
+    productInteger = helpers.addStringsOnlyIntegers(productInteger, tostring(overflow))
+
+    return productInteger .. (productFraction ~= 0 and ("." .. productFraction) or "")
+end
+
+function helpers.multiplyStringsOnlyIntegers(str1, str2)
+    str1 = helpers.rjust(str1, #str2, "0")
+    str2 = helpers.rjust(str2, #str1, "0")
+
+    local result = "0"
+
+    for i = #str2, 1, -1 do
+        local digit2 = tonumber(str2:sub(i, i))
+        local partialProduct = helpers.multiplyStringByDigit(str1, digit2)
+        result = helpers.addStringsOnlyIntegers(result, partialProduct .. string.rep("0", #str2 - i))
+    end
+
+    return result
+end
+
 function helpers.multiplyStringByDigit(str, digit)
     local carry = 0
     local result = {}
@@ -123,26 +175,11 @@ function helpers.multiplyStringByDigit(str, digit)
     return table.concat(result)
 end
 
--- Utility: Multiply two string numbers
-function helpers.multiplyStrings(str1, str2)
-    local result = "0"
 
-    for i = #str2, 1, -1 do
-        local digit = tonumber(str2:sub(i, i))
-        local partialProduct = helpers.multiplyStringByDigit(str1, digit)
-        partialProduct = partialProduct .. string.rep("0", #str2 - i)
-        result = helpers.genericAddition(result, partialProduct)
-    end
-
-    return result
-end
-
--- Utility: Zero Multiplication
 function helpers.zeroMultiplication(str1, str2)
     return (str1 == "0" or str2 == "0") and "0" or false
 end
 
--- Utility: Karatsuba multiplication
 function helpers.karatsubaMultiplication(str1, str2)
     if #str1 < 10 or #str2 < 10 then
         return helpers.multiplyStrings(str1, str2)
@@ -161,36 +198,47 @@ function helpers.karatsubaMultiplication(str1, str2)
     local z1 = helpers.karatsubaMultiplication(helpers.genericAddition(low1, high1), helpers.genericAddition(low2, high2))
     local z2 = helpers.karatsubaMultiplication(high1, high2)
 
-    return helpers.genericAddition(helpers.genericAddition(z2 .. string.rep("0", 2 * m2), helpers.subtractStrings(helpers.subtractStrings(z1, z2), z0) .. string.rep("0", m2)), z0)
+    return helpers.addStrings(helpers.addStrings(z2 .. string.rep("0", 2 * m2), helpers.subtractStrings(helpers.subtractStrings(z1, z2), z0) .. string.rep("0", m2)), z0)
 end
 
--- Utility: Divide two string numbers
 function helpers.divideStrings(str1, str2)
     if str2 == "0" then
         error("Division by zero error!")
     end
 
-    local dividend = str1
-    local divisor = str2
-    local quotient = "0"
-
-    while helpers.compareStrings(dividend, divisor) >= 0 do
-        local partialQuotient = "1"
-        local partialProduct = divisor
-
-        while helpers.compareStrings(dividend, helpers.multiplyStrings(partialProduct, "2")) >= 0 do
-            partialQuotient = helpers.multiplyStrings(partialQuotient, "2")
-            partialProduct = helpers.multiplyStrings(partialProduct, "2")
-        end
-
-        quotient = helpers.genericAddition(quotient, partialQuotient)
-        dividend = helpers.subtractStrings(dividend, partialProduct)
+    local sign = ""
+    if str1:sub(1, 1) == "-" then
+        sign = sign == "" and "-" or ""
+        str1 = str1:sub(2)
+    end
+    if str2:sub(1, 1) == "-" then
+        sign = sign == "" and "-" or ""
+        str2 = str2:sub(2)
     end
 
-    return quotient
+    local quotient = ""
+    local remainder = ""
+    local current = ""
+
+    for i = 1, #str1 do
+        current = current .. str1:sub(i, i)
+        local digit = 0
+
+        while helpers.compareStrings(current, str2) >= 0 do
+            current = helpers.subtractStringsOnlyIntegers(current, str2)
+            digit = digit + 1
+        end
+
+        remainder = current
+        quotient = quotient .. tostring(digit)
+    end
+
+    quotient = quotient:match("^0*(%d+)$") or "0"
+    remainder = remainder:match("^0*(%d+)$") or "0"
+
+    return sign .. quotient --, remainder
 end
 
--- Utility: Compare two string numbers
 function helpers.compareStrings(str1, str2)
     local len1, len2 = #str1, #str2
 
@@ -213,7 +261,6 @@ function helpers.compareStrings(str1, str2)
     end
 end
 
--- Utility: Modulus two string numbers
 function helpers.modulusStrings(str1, str2)
     if str2 == "0" then
         error("Modulus by zero error!")
@@ -237,7 +284,6 @@ function helpers.modulusStrings(str1, str2)
     return dividend
 end
 
--- Power Operation
 function helpers.powerStrings(base, exponent)
     if exponent == "0" then
         return "1"
@@ -275,7 +321,7 @@ local specialCases = {
 }
 
 function StrMath.functions.addStrings(str1, str2)
-    for _, specialCase in pairs(specialCases) do
+    for _, specialCase in specialCases do
         local result = specialCase(str1, str2)
         if result then
             return result
@@ -311,7 +357,7 @@ function StrMath.functions.multiplyStrings(str1, str2)
         sign2 = -1
     end
 
-    for _, specialCase in pairs(specialCases) do
+    for _, specialCase in specialCases do
         local result = specialCase(str1, str2)
         if result then
             return result
@@ -365,37 +411,63 @@ function StrMath.functions.powerStrings(base, exponent)
     return helpers.powerStrings(base, exponent)
 end
 
-function mt.__add(number1, number2)
-    return StrMath.new(StrMath.functions.addStrings(number1.valueString, number2.valueString))
+function StrMath.functions.compareStrings(str1, str2)
+    return helpers.compareStrings(str1, str2)
 end
 
-function mt.__sub(number1, number2)
-    return StrMath.new(StrMath.functions.subtractStrings(number1.valueString, number2.valueString))
+function StrMath:__add(other)
+    return StrMath.new(StrMath.functions.addStrings(self.valueString, other.valueString))
 end
 
-function mt.__mul(number1, number2)
-    return StrMath.new(StrMath.functions.multiplyStrings(number1.valueString, number2.valueString))
+function StrMath:__sub(other)
+    return StrMath.new(StrMath.functions.subtractStrings(self.valueString, other.valueString))
 end
 
-function mt.__div(number1, number2)
-    local result, err = StrMath.functions.divideStrings(number1.valueString, number2.valueString)
-    if err then
-        error(err) 
-    end
-    return StrMath.new(result)
+function StrMath:__mul(other)
+    return StrMath.new(StrMath.functions.multiplyStrings(self.valueString, other.valueString))
 end
 
-function mt.__mod(number1, number2)
-    return StrMath.new(StrMath.functions.modulusStrings(number1.valueString, number2.valueString))
+function StrMath:__div(other)
+    return StrMath.new(StrMath.functions.divideStrings(self.valueString, other.valueString))
 end
 
-function mt.__pow(base, exponent)
-    return StrMath.new(StrMath.functions.powerStrings(base.valueString, exponent.valueString))
+function StrMath:__mod(other)
+    return StrMath.new(StrMath.functions.modulusStrings(self.valueString, other.valueString))
 end
 
-function mt.__tostring(number)
-    return number.valueString
+function StrMath:__pow(other)
+    return StrMath.new(StrMath.functions.powerStrings(self.valueString, other.valueString))
 end
+
+function StrMath:__eq(other)
+    return self.valueString == other.valueString
+end
+
+function StrMath:__lt(other)
+    return StrMath.functions.compareStrings(self.valueString, other.valueString) < 0
+end
+
+function StrMath:__le(other)
+    return StrMath.functions.compareStrings(self.valueString, other.valueString) <= 0
+end
+
+function StrMath:__tostring()
+    return self.valueString
+end
+
+mt.__index = StrMath
+mt.__add = StrMath.__add
+mt.__sub = StrMath.__sub
+mt.__mul = StrMath.__mul
+mt.__div = StrMath.__div
+mt.__mod = StrMath.__mod
+mt.__pow = StrMath.__pow
+mt.__eq = StrMath.__eq
+mt.__lt = StrMath.__lt
+mt.__le = StrMath.__le
+mt.__tostring = StrMath.__tostring
+
+
 mt.__index = StrMath
 mt.__metatable = "protected"
 
