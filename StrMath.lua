37fd4608ff9593730987178b8989dc951c30e05c
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-08-19 07:57:56 -0400

    Update StrMath.lua

--- StrMath.lua
+++ StrMath.lua
@@ -7,57 +7,57 @@ local mt = {}
 -- Helper Functions Dictionary
 local helpers = {}
 
-function helpers.reverseString(s)
-    return s:reverse()
+function helpers.ReverseString(s)
+    return s:Reverse()
 end
 
-function helpers.rjust(str, length, char)
+function helpers.RJust(str, length, char)
     char = char or "0"
     return string.rep(char, length - #str) .. str
 end
 
-function helpers.validateNumber(str)
+function helpers.ValidateNumber(str)
     if not str:match("^-?%d+%.?%d*$") then
         error("Invalid number string: " .. str)
     end
 end
 
-function helpers.splitNumber(str)
+function helpers.SplitNumber(str)
     local integerPart, fractionalPart = str:match("^(-?%d+)(%.%d*)$")
     fractionalPart = fractionalPart or ""
     fractionalPart = fractionalPart:sub(2) -- remove the dot
     return integerPart, fractionalPart
 end
 
-function helpers.addStrings(str1, str2)
-    local integer1, fraction1 = helpers.splitNumber(str1)
-    local integer2, fraction2 = helpers.splitNumber(str2)
+function helpers.AddStrings(str1, str2)
+    local integer1, fraction1 = helpers.SplitNumber(str1)
+    local integer2, fraction2 = helpers.SplitNumber(str2)
 
-    local sumFraction = helpers.addStringsOnlyIntegers(fraction1, fraction2)
+    local sumFraction = helpers.AddStringsOnlyIntegers(fraction1, fraction2)
     local overflow = math.floor(sumFraction / (10 ^ #fraction1))
     sumFraction = sumFraction % (10 ^ #fraction1)
 
-    local sumInteger = helpers.addStringsOnlyIntegers(integer1, integer2) + overflow
+    local sumInteger = helpers.AddStringsOnlyIntegers(integer1, integer2) + overflow
 
     return sumInteger .. (sumFraction ~= 0 and ("." .. sumFraction) or "")
 end
 
 function helpers.genericAddition(str1, str2)
     if str1:sub(1, 1) == "-" and str2:sub(1, 1) ~= "-" then
-        return helpers.subtractStrings(str2, str1:sub(2))
+        return helpers.SubtractStrings(str2, str1:sub(2))
     elseif str1:sub(1, 1) ~= "-" and str2:sub(1, 1) == "-" then
-        return helpers.subtractStrings(str1, str2:sub(2))
+        return helpers.SubtractStrings(str1, str2:sub(2))
     elseif str1:sub(1, 1) == "-" and str2:sub(1, 1) == "-" then
-        return "-" .. helpers.addStrings(str1:sub(2), str2:sub(2))
+        return "-" .. helpers.AddStrings(str1:sub(2), str2:sub(2))
     else
-        return helpers.addStrings(str1, str2)
+        return helpers.AddStrings(str1, str2)
     end
 end
 
-function helpers.addStringsOnlyIntegers(str1, str2)
+function helpers.AddStringsOnlyIntegers(str1, str2)
     local length = math.max(#str1, #str2)
-    str1 = str1:rjust(length, "0")
-    str2 = str2:rjust(length, "0")
+    str1 = str1:RJust(length, "0")
+    str2 = str2:RJust(length, "0")
 
     local result = {}
     local carry = 0
@@ -75,9 +75,9 @@ function helpers.addStringsOnlyIntegers(str1, str2)
     return table.concat(result)
 end
 
-function helpers.subtractStrings(str1, str2)
-    local integer1, fraction1 = helpers.splitNumber(str1)
-    local integer2, fraction2 = helpers.splitNumber(str2)
+function helpers.SubtractStrings(str1, str2)
+    local integer1, fraction1 = helpers.SplitNumber(str1)
+    local integer2, fraction2 = helpers.SplitNumber(str2)
 
     if #fraction2 > #fraction1 then
         fraction1 = fraction1 .. string.rep("0", #fraction2 - #fraction1)
@@ -85,7 +85,7 @@ function helpers.subtractStrings(str1, str2)
         fraction2 = fraction2 .. string.rep("0", #fraction1 - #fraction2)
     end
 
-    local subFraction = helpers.subtractStringsOnlyIntegers(fraction1, fraction2)
+    local subFraction = helpers.SubtractStringsOnlyIntegers(fraction1, fraction2)
     local borrow = 0
 
     if subFraction < 0 then
@@ -93,15 +93,15 @@ function helpers.subtractStrings(str1, str2)
         borrow = 1
     end
 
-    local subInteger = helpers.subtractStringsOnlyIntegers(integer1, integer2) - borrow
+    local subInteger = helpers.SubtractStringsOnlyIntegers(integer1, integer2) - borrow
 
     return subInteger .. (subFraction ~= 0 and ("." .. subFraction) or "")
 end
 
-function helpers.subtractStringsOnlyIntegers(str1, str2)
+function helpers.SubtractStringsOnlyIntegers(str1, str2)
     local length = math.max(#str1, #str2)
-    str1 = helpers.rjust(str1, length, "0")
-    str2 = helpers.rjust(str2, length, "0")    
+    str1 = helpers.RJust(str1, length, "0")
+    str2 = helpers.RJust(str2, length, "0")    
 
     local result = {}
     local borrow = 0
@@ -123,42 +123,42 @@ function helpers.subtractStringsOnlyIntegers(str1, str2)
     return tonumber(finalResult)
 end
 
-function helpers.multiplyStrings(str1, str2)
-    local integer1, fraction1 = helpers.splitNumber(str1)
-    local integer2, fraction2 = helpers.splitNumber(str2)
+function helpers.MultiplyStrings(str1, str2)
+    local integer1, fraction1 = helpers.SplitNumber(str1)
+    local integer2, fraction2 = helpers.SplitNumber(str2)
 
-    local productInteger = helpers.multiplyStringsOnlyIntegers(integer1, integer2)
-    local productFraction1 = helpers.multiplyStringsOnlyIntegers(integer1, fraction2)
-    local productFraction2 = helpers.multiplyStringsOnlyIntegers(integer2, fraction1)
-    local productFraction3 = helpers.multiplyStringsOnlyIntegers(fraction1, fraction2)
+    local productInteger = helpers.MultiplyStringsOnlyIntegers(integer1, integer2)
+    local productFraction1 = helpers.MultiplyStringsOnlyIntegers(integer1, fraction2)
+    local productFraction2 = helpers.MultiplyStringsOnlyIntegers(integer2, fraction1)
+    local productFraction3 = helpers.MultiplyStringsOnlyIntegers(fraction1, fraction2)
 
     local fractionalLength = #fraction1 + #fraction2
-    local productFraction = helpers.addStringsOnlyIntegers(productFraction1 .. productFraction2, productFraction3)
+    local productFraction = helpers.AddStringsOnlyIntegers(productFraction1 .. productFraction2, productFraction3)
 
     local overflow = math.floor(productFraction / (10 ^ fractionalLength))
     productFraction = productFraction % (10 ^ fractionalLength)
 
-    productInteger = helpers.addStringsOnlyIntegers(productInteger, tostring(overflow))
+    productInteger = helpers.AddStringsOnlyIntegers(productInteger, tostring(overflow))
 
     return productInteger .. (productFraction ~= 0 and ("." .. productFraction) or "")
 end
 
-function helpers.multiplyStringsOnlyIntegers(str1, str2)
-    str1 = helpers.rjust(str1, #str2, "0")
-    str2 = helpers.rjust(str2, #str1, "0")
+function helpers.MultiplyStringsOnlyIntegers(str1, str2)
+    str1 = helpers.RJust(str1, #str2, "0")
+    str2 = helpers.RJust(str2, #str1, "0")
 
     local result = "0"
 
     for i = #str2, 1, -1 do
         local digit2 = tonumber(str2:sub(i, i))
-        local partialProduct = helpers.multiplyStringByDigit(str1, digit2)
-        result = helpers.addStringsOnlyIntegers(result, partialProduct .. string.rep("0", #str2 - i))
+        local partialProduct = helpers.MultiplyStringByDigit(str1, digit2)
+        result = helpers.AddStringsOnlyIntegers(result, partialProduct .. string.rep("0", #str2 - i))
     end
 
     return result
 end
 
-function helpers.multiplyStringByDigit(str, digit)
+function helpers.MultiplyStringByDigit(str, digit)
     local carry = 0
     local result = {}
 
@@ -176,13 +176,13 @@ function helpers.multiplyStringByDigit(str, digit)
 end
 
 
-function helpers.zeroMultiplication(str1, str2)
+function helpers.ZeroMultiplication(str1, str2)
     return (str1 == "0" or str2 == "0") and "0" or false
 end
 
-function helpers.karatsubaMultiplication(str1, str2)
+function helpers.KaratsubaMultiplication(str1, str2)
     if #str1 < 10 or #str2 < 10 then
-        return helpers.multiplyStrings(str1, str2)
+        return helpers.MultiplyStrings(str1, str2)
     end
 
     local m = math.max(#str1, #str2)
@@ -194,16 +194,16 @@ function helpers.karatsubaMultiplication(str1, str2)
     local high2 = str2:sub(1, #str2 - m2)
     local low2 = str2:sub(#str2 - m2 + 1)
 
-    local z0 = helpers.karatsubaMultiplication(low1, low2)
-    local z1 = helpers.karatsubaMultiplication(helpers.genericAddition(low1, high1), helpers.genericAddition(low2, high2))
-    local z2 = helpers.karatsubaMultiplication(high1, high2)
+    local z0 = helpers.KaratsubaMultiplication(low1, low2)
+    local z1 = helpers.KaratsubaMultiplication(helpers.genericAddition(low1, high1), helpers.genericAddition(low2, high2))
+    local z2 = helpers.KaratsubaMultiplication(high1, high2)
 
-    return helpers.addStrings(helpers.addStrings(z2 .. string.rep("0", 2 * m2), helpers.subtractStrings(helpers.subtractStrings(z1, z2), z0) .. string.rep("0", m2)), z0)
+    return helpers.AddStrings(helpers.AddStrings(z2 .. string.rep("0", 2 * m2), helpers.SubtractStrings(helpers.SubtractStrings(z1, z2), z0) .. string.rep("0", m2)), z0)
 end
 
-function helpers.divideStrings(str1, str2)
+function helpers.DivideStrings(str1, str2)
     if str2 == "0" then
-        error("Division by zero error!")
+        error("Division by Zero error!")
     end
 
     local sign = ""
@@ -224,8 +224,8 @@ function helpers.divideStrings(str1, str2)
         current = current .. str1:sub(i, i)
         local digit = 0
 
-        while helpers.compareStrings(current, str2) >= 0 do
-            current = helpers.subtractStringsOnlyIntegers(current, str2)
+        while helpers.CompareStrings(current, str2) >= 0 do
+            current = helpers.SubtractStringsOnlyIntegers(current, str2)
             digit = digit + 1
         end
 
@@ -239,7 +239,7 @@ function helpers.divideStrings(str1, str2)
     return sign .. quotient --, remainder
 end
 
-function helpers.compareStrings(str1, str2)
+function helpers.CompareStrings(str1, str2)
     local len1, len2 = #str1, #str2
 
     if len1 > len2 then
@@ -261,30 +261,30 @@ function helpers.compareStrings(str1, str2)
     end
 end
 
-function helpers.modulusStrings(str1, str2)
+function helpers.ModulusStrings(str1, str2)
     if str2 == "0" then
-        error("Modulus by zero error!")
+        error("Modulus by Zero error!")
     end
 
-    local dividend = str1
+    local Dividend = str1
     local divisor = str2
 
-    while helpers.compareStrings(dividend, divisor) >= 0 do
+    while helpers.CompareStrings(Dividend, divisor) >= 0 do
         local partialQuotient = "1"
         local partialProduct = divisor
 
-        while helpers.compareStrings(dividend, helpers.multiplyStrings(partialProduct, "2")) >= 0 do
-            partialQuotient = helpers.multiplyStrings(partialQuotient, "2")
-            partialProduct = helpers.multiplyStrings(partialProduct, "2")
+        while helpers.CompareStrings(Dividend, helpers.MultiplyStrings(partialProduct, "2")) >= 0 do
+            partialQuotient = helpers.MultiplyStrings(partialQuotient, "2")
+            partialProduct = helpers.MultiplyStrings(partialProduct, "2")
         end
 
-        dividend = helpers.subtractStrings(dividend, partialProduct)
+        Dividend = helpers.SubtractStrings(Dividend, partialProduct)
     end
 
-    return dividend
+    return Dividend
 end
 
-function helpers.powerStrings(base, exponent)
+function helpers.PowerStrings(base, exponent)
     if exponent == "0" then
         return "1"
     elseif base == "0" then
@@ -292,7 +292,7 @@ function helpers.powerStrings(base, exponent)
     else
         local result = "1"
         for _ = 1, tonumber(exponent) do
-            result = helpers.multiplyStrings(result, base)
+            result = helpers.MultiplyStrings(result, base)
         end
         return result
     end
@@ -300,27 +300,27 @@ end
 
 -- Special Cases Dictionary
 local specialCases = {
-    addSingleDigit = function(str1, str2)
-        return #str2 == 1 and helpers.addSingleDigit(str1, str2) or false
+    AddSingleDigit = function(str1, str2)
+        return #str2 == 1 and helpers.AddSingleDigit(str1, str2) or false
     end,
-    addSelf = function(str1, str2)
-        return str1 == str2 and helpers.addSelf(str1) or false
+    AddSelf = function(str1, str2)
+        return str1 == str2 and helpers.AddSelf(str1) or false
     end,
-    zeroAddition = function(str1, str2)
+    ZeroAddition = function(str1, str2)
         return (str1 == "0" and str2) or (str2 == "0" and str1) or false
     end,
-    zeroMultiplication = function(str1, str2)
+    ZeroMultiplication = function(str1, str2)
         return (str1 == "0" or str2 == "0") and "0" or false
     end,
-    zeroDivision = function(str1, str2)
+    ZeroDivision = function(str1, str2)
         return str2 == "0" and "NaN" or false
     end,
-    zeroSubtraction = function(str1, str2)
+    ZeroSubtraction = function(str1, str2)
         return str1 == str2 and "0" or false
     end,
 }
 
-function StrMath.functions.addStrings(str1, str2)
+function StrMath.functions.AddStrings(str1, str2)
     for _, specialCase in specialCases do
         local result = specialCase(str1, str2)
         if result then
@@ -330,19 +330,19 @@ function StrMath.functions.addStrings(str1, str2)
     return helpers.genericAddition(str1, str2)
 end
 
-function StrMath.functions.subtractStrings(str1, str2)
-    local result = specialCases.zeroSubtraction(str1, str2)
+function StrMath.functions.SubtractStrings(str1, str2)
+    local result = specialCases.ZeroSubtraction(str1, str2)
     if result then
         return result
     end
-    return helpers.subtractStrings(str1, str2)
+    return helpers.SubtractStrings(str1, str2)
 end
 
-function StrMath.functions.multiplyStrings(str1, str2)
-    helpers.validateNumber(str1)
-    helpers.validateNumber(str2)
+function StrMath.functions.MultiplyStrings(str1, str2)
+    helpers.ValidateNumber(str1)
+    helpers.ValidateNumber(str2)
 
-    local result = specialCases.zeroMultiplication(str1, str2)
+    local result = specialCases.ZeroMultiplication(str1, str2)
     if result then
         return result
     end
@@ -364,23 +364,23 @@ function StrMath.functions.multiplyStrings(str1, str2)
         end
     end
 
-    local result = helpers.karatsubaMultiplication(str1, str2)
+    local result = helpers.KaratsubaMultiplication(str1, str2)
     if sign1 * sign2 < 0 then
         result = "-" .. result
     end
     return result
 end
 
-function StrMath.functions.divideStrings(str1, str2)
-    local result = specialCases.zeroDivision(str1, str2)
+function StrMath.functions.DivideStrings(str1, str2)
+    local result = specialCases.ZeroDivision(str1, str2)
     if result then
         return result
     end
 
     if str2 == "0" then
-        return nil, "Division by zero" -- Handle division by zero
+        return nil, "Division by Zero" -- Handle division by Zero
     end
-    return helpers.divideStrings(str1, str2)
+    return helpers.DivideStrings(str1, str2)
 end
 
 function StrMath.new(valueString)
@@ -388,55 +388,55 @@ function StrMath.new(valueString)
     return setmetatable(instance, mt)
 end
 
-function StrMath.functions.subtractStrings(str1, str2)
-    return helpers.subtractStrings(str1, str2)
+function StrMath.functions.SubtractStrings(str1, str2)
+    return helpers.SubtractStrings(str1, str2)
 end
 
-function StrMath.functions.multiplyStrings(str1, str2)
-    return helpers.multiplyStrings(str1, str2)
+function StrMath.functions.MultiplyStrings(str1, str2)
+    return helpers.MultiplyStrings(str1, str2)
 end
 
-function StrMath.functions.divideStrings(str1, str2)
+function StrMath.functions.DivideStrings(str1, str2)
     if str2 == "0" then
-        return nil, "Division by zero" -- Handle division by zero
+        return nil, "Division by Zero" -- Handle division by Zero
     end
-    return helpers.divideStrings(str1, str2)
+    return helpers.DivideStrings(str1, str2)
 end
 
-function StrMath.functions.modulusStrings(str1, str2)
-    return helpers.modulusStrings(str1, str2)
+function StrMath.functions.ModulusStrings(str1, str2)
+    return helpers.ModulusStrings(str1, str2)
 end
 
-function StrMath.functions.powerStrings(base, exponent)
-    return helpers.powerStrings(base, exponent)
+function StrMath.functions.PowerStrings(base, exponent)
+    return helpers.PowerStrings(base, exponent)
 end
 
-function StrMath.functions.compareStrings(str1, str2)
-    return helpers.compareStrings(str1, str2)
+function StrMath.functions.CompareStrings(str1, str2)
+    return helpers.CompareStrings(str1, str2)
 end
 
 function StrMath:__add(other)
-    return StrMath.new(StrMath.functions.addStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.AddStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__sub(other)
-    return StrMath.new(StrMath.functions.subtractStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.SubtractStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__mul(other)
-    return StrMath.new(StrMath.functions.multiplyStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.MultiplyStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__div(other)
-    return StrMath.new(StrMath.functions.divideStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.DivideStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__mod(other)
-    return StrMath.new(StrMath.functions.modulusStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.ModulusStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__pow(other)
-    return StrMath.new(StrMath.functions.powerStrings(self.valueString, other.valueString))
+    return StrMath.new(StrMath.functions.PowerStrings(self.valueString, other.valueString))
 end
 
 function StrMath:__eq(other)
@@ -444,11 +444,11 @@ function StrMath:__eq(other)
 end
 
 function StrMath:__lt(other)
-    return StrMath.functions.compareStrings(self.valueString, other.valueString) < 0
+    return StrMath.functions.CompareStrings(self.valueString, other.valueString) < 0
 end
 
 function StrMath:__le(other)
-    return StrMath.functions.compareStrings(self.valueString, other.valueString) <= 0
+    return StrMath.functions.CompareStrings(self.valueString, other.valueString) <= 0
 end
 
 function StrMath:__tostring()
@@ -456,7 +456,7 @@ function StrMath:__tostring()
 end
 
 mt.__index = StrMath
-mt.__add = StrMath.__add
+mt.__Add = StrMath.__Add
 mt.__sub = StrMath.__sub
 mt.__mul = StrMath.__mul
 mt.__div = StrMath.__div
