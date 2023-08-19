a7fad1505fde4614c8d13d9713e192bc02c3b27a
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-08-19 07:54:31 -0400

    Update Scalars.lua

--- Scalars.lua
+++ Scalars.lua
@@ -10,39 +10,39 @@ end
 local Complex = {}
 Complex.__index = Complex
 
-function Complex:new(real, imaginary)
+function Complex:New(real, imaginary)
   return setmetatable({real = real or 0, imaginary = imaginary or 0}, self)
 end
 
-function Complex:add(other)
-  return Complex:new(self.real + other.real, self.imaginary + other.imaginary)
+function Complex:Add(other)
+  return Complex:New(self.real + other.real, self.imaginary + other.imaginary)
 end
 
-function Complex:subtract(other)
-  return Complex:new(self.real - other.real, self.imaginary - other.imaginary)
+function Complex:Subtract(other)
+  return Complex:New(self.real - other.real, self.imaginary - other.imaginary)
 end
 
-function Complex:multiply(other)
+function Complex:Multiply(other)
   local real = self.real * other.real - self.imaginary * other.imaginary
   local imaginary = self.real * other.imaginary + self.imaginary * other.real
-  return Complex:new(real, imaginary)
+  return Complex:New(real, imaginary)
 end
 
-function Complex:divide(other)
+function Complex:Divide(other)
   local denom = other.real^2 + other.imaginary^2
   local real = (self.real * other.real + self.imaginary * other.imaginary) / denom
   local imaginary = (self.imaginary * other.real - self.real * other.imaginary) / denom
-  return Complex:new(real, imaginary)
+  return Complex:New(real, imaginary)
 end
 
-function Complex:magnitude()
-  return math.sqrt(self.real^2 + self.imaginary^2)
+function Complex:Magnitude()
+  return math.Sqrt(self.real^2 + self.imaginary^2)
 end
 
-function Complex:toPolar()
-  local magnitude = self:magnitude()
+function Complex:ToPolar()
+  local Magnitude = self:Magnitude()
   local angle = math.atan2(self.imaginary, self.real)
-  return magnitude, angle
+  return Magnitude, angle
 end
 
 function Complex:__tostring()
@@ -52,7 +52,7 @@ end
 
 local Rational = {}
 
-function Rational:new(numerator, denominator)
+function Rational:New(numerator, denominator)
     local common_divisor = gcd(numerator, denominator)
     return setmetatable({
       numerator = numerator / common_divisor,
@@ -60,24 +60,24 @@ function Rational:new(numerator, denominator)
     }, self)
 end
 
-function Rational:add(other)
+function Rational:Add(other)
     local numerator = self.numerator * other.denominator + other.numerator * self.denominator
     local denominator = self.denominator * other.denominator
-    return Rational:new(numerator, denominator)
+    return Rational:New(numerator, denominator)
 end
 
-function Rational:subtract(other)
+function Rational:Subtract(other)
     local numerator = self.numerator * other.denominator - other.numerator * self.denominator
     local denominator = self.denominator * other.denominator
-    return Rational:new(numerator, denominator)
+    return Rational:New(numerator, denominator)
 end
 
-function Rational:multiply(other)
-    return Rational:new(self.numerator * other.numerator, self.denominator * other.denominator)
+function Rational:Multiply(other)
+    return Rational:New(self.numerator * other.numerator, self.denominator * other.denominator)
 end
 
-function Rational:divide(other)
-    return Rational:new(self.numerator * other.denominator, self.denominator * other.numerator)
+function Rational:Divide(other)
+    return Rational:New(self.numerator * other.denominator, self.denominator * other.numerator)
 end
 
 function Rational:toDecimal()
@@ -91,23 +91,23 @@ end
 local ZmodP = {}
 ZmodP.__index = ZmodP
 
-function ZmodP:new(value, modulus)
+function ZmodP:New(value, modulus)
     return setmetatable({value = value % modulus, modulus = modulus}, self)
 end
 
-function ZmodP:add(other)
-    return ZmodP:new(self.value + other.value, self.modulus)
+function ZmodP:Add(other)
+    return ZmodP:New(self.value + other.value, self.modulus)
 end
 
-function ZmodP:subtract(other)
-    return ZmodP:new(self.value - other.value, self.modulus)
+function ZmodP:Subtract(other)
+    return ZmodP:New(self.value - other.value, self.modulus)
 end
 
-function ZmodP:multiply(other)
-    return ZmodP:new(self.value * other.value, self.modulus)
+function ZmodP:Multiply(other)
+    return ZmodP:New(self.value * other.value, self.modulus)
 end
 
-function ZmodP:exponential(power)
+function ZmodP:Exponential(power)
     local result = 1
     local base = self.value
     while power > 0 do
@@ -117,17 +117,17 @@ function ZmodP:exponential(power)
       base = (base * base) % self.modulus
       power = math.floor(power / 2)
     end
-    return ZmodP:new(result, self.modulus)
+    return ZmodP:New(result, self.modulus)
 end
 
-function ZmodP:inverse()
-    local r, new_r = self.modulus, self.value
-    local t, new_t = 0, 1
+function ZmodP:Inverse()
+    local r, New_r = self.modulus, self.value
+    local t, New_t = 0, 1
 
-    while new_r ~= 0 do
-      local quotient = math.floor(r / new_r)
-      r, new_r = new_r, r - quotient * new_r
-      t, new_t = new_t, t - quotient * new_t
+    while New_r ~= 0 do
+      local quotient = math.floor(r / New_r)
+      r, New_r = New_r, r - quotient * New_r
+      t, New_t = New_t, t - quotient * New_t
     end
 
     if t < 0 then t = t + self.modulus end
@@ -136,7 +136,7 @@ end
 
 local ScalarMaths = {}
 
-function ScalarMaths:sqrt(value)
+function ScalarMaths:Sqrt(value)
     local guess = value / 2
     while math.abs(guess * guess - value) > 1e-9 do
       guess = (guess + value / guess) / 2
@@ -144,23 +144,23 @@ function ScalarMaths:sqrt(value)
     return guess
 end
 
-function ScalarMaths:sin(value)
+function ScalarMaths:Sin(value)
     local sum = 0
     for n = 0, 10 do
-      sum = sum + ((-1)^n * value^(2 * n + 1)) / factorial(2 * n + 1)
+      sum = sum + ((-1)^n * value^(2 * n + 1)) / Factorial(2 * n + 1)
     end
     return sum
 end
 
-function ScalarMaths:cos(value)
+function ScalarMaths:Cos(value)
     local sum = 0
     for n = 0, 10 do
-      sum = sum + ((-1)^n * value^(2 * n)) / factorial(2 * n)
+      sum = sum + ((-1)^n * value^(2 * n)) / Factorial(2 * n)
     end
     return sum
 end
 
-function ScalarMaths:log(value)
+function ScalarMaths:Log(value)
     local sum = 0
     for n = 1, 1000 do
       sum = sum + (1 / n) * ((value - 1)^n)
@@ -168,9 +168,9 @@ function ScalarMaths:log(value)
    return sum
 end
 
-  -- Helper function to calculate the factorial
-function factorial(n)
-    return n == 0 and 1 or n * factorial(n - 1)
+  -- Helper function to calculate the Factorial
+function Factorial(n)
+    return n == 0 and 1 or n * Factorial(n - 1)
 end
 
 ScalarMaths.Complex = Complex
